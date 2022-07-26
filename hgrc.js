3199fbb285bfbbc9fecbe8213f45285fa7d7a9a5
Eyelly Wu <eyelly.wu@gmail.com>
2022-07-26 21:59:17 +0800

    feat: add basic implementation

---
+++ hgrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  outputDir: __dirname,
+  catalogues: [
+    {
+      name: 'github',
+      domainNames: [
+        {
+          name: 'github.com',
+          description:'home page',
+        },
+        'collector.github.com',
+        'api.github.com',
+        'spotlights-feed.github.com',
+        'events-feed.github.com',
+        'gist.github.com',
+        'central.github.com',
+        'assets-cdn.github.com',
+        'codeload.github.com',
+        'raw.github.com',
+        {
+          name: 'github.io',
+          description: 'github pages',
+        },
+        {
+          name: 'github.dev',
+          description: 'web editor',
+        },
+        {
+          name: 'github.githubassets.com',
+          description: 'assets',
+        },
+        {
+          name: 'avatars.githubusercontent.com',
+          description: 'avatars',
+        },
+        'avatars0.githubusercontent.com',
+        'avatars1.githubusercontent.com',
+        'avatars2.githubusercontent.com',
+        'avatars3.githubusercontent.com',
+        'avatars4.githubusercontent.com',
+        'avatars5.githubusercontent.com',
+        'favicons.githubusercontent.com',
+        'user-images.githubusercontent.com',
+        'raw.githubusercontent.com',
+        'marketplace-screenshots.githubusercontent.com',
+        'desktop.githubusercontent.com',
+        'camo.githubusercontent.com',
+        'media.githubusercontent.com',
+        'objects.githubusercontent.com',
+        'copilot-proxy.githubusercontent.com',
+      ],
+    },
+  ],
+}
